c34244cb695e2ed69af625f7c10acf15db45c031
germain loret <germain.loret@gmail.com>
2020-08-10 17:55:42 +0200

    Implement action index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "flats", to: "flats#index"
+  get "flats/:id", to: "flats#show", as: "flat"
+  # flats/145
+  # flats/201
 end
